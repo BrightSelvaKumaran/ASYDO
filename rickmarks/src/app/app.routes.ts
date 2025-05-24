819772687e8e57631dac9a8465e90160650d72bb
Bright Selva Kumaran <110169833+BrightSelvaKumaran@users.noreply.github.com>
2025-05-24 18:17:51 +0530

    first update

---
+++ rickmarks/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { HomeLandingComponent } from './pages/home-landing/home-landing.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeLandingComponent
+  },
+  {
+    path: 'homeLnading',
+    component: HomeLandingComponent
+  }
+];
---
+++ rickmarks/src/app/pages/home-landing/home-landing.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-landing',
+  imports: [],
+  templateUrl: './home-landing.component.html',
+  styleUrl: './home-landing.component.css'
+})
+export class HomeLandingComponent {
+
+}
